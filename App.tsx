58ef90997a746b2388612676e844c8d47731a660
marius-ck <marius.pasca@callstack.com>
2024-10-25 13:02:57 +0300

    Add msw for endpoints data mocking

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Navigation from './src/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
 
 const queryClient = new QueryClient()
 
 function App(): React.JSX.Element {
+  const [loading, setLoading] = React.useState(true);
+
+  async function enableMocking() {
+    if (!__DEV__) {
+      return
+    }
+
+    await import('./msw.polyfills')
+    const { server } = await import('./src/api/mocks/server')
+    server.listen()
+  }
+
+  useEffect(() => {
+    enableMocking().then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Text>Loading MSW</Text>;
+  }
 
   return (
     <SafeAreaProvider>
--- src/api/weather/index.tsx
+++ src/api/weather/index.tsx
@@ -2,9 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { API_KEY } from "@env"
 import { WeatherData } from "./types";
 
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/group';
+
 const fetchGroupCities = async (cityIds: number[]): Promise<WeatherData[]> => {
     const cityIdsString = cityIds.join(',');
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/group?id=${cityIdsString}&appid=${API_KEY}&units=imperial`);
+    const response = await fetch(`${WEATHER_API_URL}?id=${cityIdsString}&appid=${API_KEY}&units=imperial`);
 
     if (!response.ok) {
         throw new Error('Failed to fetch weather data');
